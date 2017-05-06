2b9308c1a4ae41b96bfc6be5658e7b37ca231173
Rick Beerendonk <rick@beerendonk.com>
2017-05-06 12:48:06 +0200

    Add bindActionCreators example

---
+++ 03 - Action Creator - bindActionCreators/systemjs - babel/main.js
@@ -0,0 +1,30 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2017 Rick Beerendonk   !*/
+
+import { bindActionCreators, createStore } from 'redux';
+
+const CHANGE_NAME = 'CHANGE_NAME';
+
+function changeName(name) {
+  return {
+    type: CHANGE_NAME,
+    payload: name
+  }
+}
+
+function reducer(state = {name: 'Reducer'}, action) { 
+  switch (action.type) {
+    case CHANGE_NAME:
+      return Object.assign({}, state, {name: action.payload});
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+const boundChangeName = bindActionCreators(changeName, store.dispatch);
+
+// Can be passed to places that don't know about store / Redux:
+boundChangeName('boundActionCreator');
+
+document.getElementById('content').innerText = store.getState().name;
---
+++ 03 - Action Creator - bindActionCreators/systemjs - typescript/main.ts
@@ -0,0 +1,38 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2017 Rick Beerendonk   !*/
+
+import { Action, bindActionCreators, createStore, Store } from 'redux';
+
+interface PayloadAction<T> extends Action {
+  payload: T;
+}
+
+const CHANGE_NAME = 'CHANGE_NAME';
+
+function changeName(name: string): PayloadAction<string> {
+  return {
+    type: CHANGE_NAME,
+    payload: name
+  }
+}
+
+interface State {
+  name: string;
+}
+
+function reducer(state = {name: 'Reducer'}, action: Action): State { 
+  switch (action.type) {
+    case CHANGE_NAME:
+      return Object.assign({}, state, {name: (<PayloadAction<string>>action).payload});
+    default:
+      return state;
+  }
+}
+
+const store: Store<State> = createStore(reducer);
+const boundChangeName: (name: string) => PayloadAction<string> = bindActionCreators(changeName, store.dispatch);
+
+// Can be passed to places that don't know about store / Redux:
+boundChangeName('boundActionCreator');
+
+document.getElementById('content').innerText = store.getState().name;
