b691c7776a78d01ad9e47405400acad4d3dcff11
Rick Beerendonk <rick@beerendonk.com>
2019-03-08 00:48:57 +0100

    Upgrade versions.

--- Extra 01 - React - b. redux-react-hook/systemjs - babel/containers/GreetingEditor.jsx
+++ Extra 01 - React - b. redux-react-hook/systemjs - babel/containers/GreetingEditor.jsx
@@ -18,8 +18,9 @@ function GreetingEditor() {
   const { name } = useMappedState(mapState);
 
   const dispatch = useDispatch();
-  const handleNameChange = React.useCallback(newName =>
-    dispatch(changeName(newName))
+  const handleNameChange = React.useCallback(
+    newName => dispatch(changeName(newName)),
+    [dispatch]
   );
 
   return (
