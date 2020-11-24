c28963118160d2edc5e6c3b03442a5b77f1ebfc6
Rick Beerendonk <rick@beerendonk.com>
2020-11-24 13:59:09 +0100

    Improve server and demo experience.

--- Util - 01 - Custom servers/server.js
+++ Util - 01 - Custom servers/server.js
@@ -7,15 +7,8 @@
 const childProcess = require('child_process');
 const path = require('path');
 
-const portHttp = process.argv[2] || 8080;
-//const portRest = +portHttp + 1;
-let filePath = process.argv[3];
+const [, , portHttp = 8080, filePath] = process.argv;
 const basePath = process.cwd();
-
-//console.log('portHttp: ' + portHttp);
-//console.log('portRest: ' + portRest);
-//console.log('filePath: ' + filePath);
-//console.log('basePath: ' + basePath);
 
 // If filePath is given, we'll open that path in the browser
 let extraUri = '';
@@ -27,35 +20,31 @@ if (filePath && filePath.toLowerCase().startsWith(basePath.toLowerCase())) {
 const serverUri = `http://localhost:${portHttp}`;
 const totalUri = `${serverUri}${extraUri}${
   extraUri[extraUri.length - 1] !== '/' && '/'
-}`;
-
-//console.log('serverUri: ' + serverUri);
-//console.log('extraPath: ' + extraPath);
-//console.log('extraUri: ' + extraUri);
-//console.log('totalUri: ' + totalUri);
+}`.replace(/\s+/g, '%20');
 
 // Start server
 // See: https://www.npmjs.com/package/serve
 /* const httpChild = */ childProcess.spawn(
   'serve',
-  [
-    '--listen',
-    portHttp,
-    '--config',
-    //'".\\Util - 01 - Custom servers\\serve.json"'
-    `"${path.join(__dirname, 'serve.json')}"`
-  ],
+  ['--listen', portHttp, '--config', `"${path.join(__dirname, 'serve.json')}"`],
   { shell: true, stdio: 'inherit' }
 );
 
 // Open browser
-const command =
-  process.platform === 'win32'
-    ? `start "" "${totalUri}"`
-    : /* process.platform === "darwin" */ `open "${totalUri}"`;
-/*const browserChild =*/ childProcess.spawn(command, {
-  shell: true,
-  stdio: 'inherit'
-});
+// Use timeout so the server is ready.
+setTimeout(function () {
+  const command =
+    process.platform === 'win32'
+      ? `start "" "${totalUri}"`
+      : /* process.platform === "darwin" */ `open "${totalUri}"`;
 
-//childProcess.exec(command, () => { console.log('exit'); process.exit(); });
+  /*const browserChild = */ childProcess.spawn(command, {
+    shell: true,
+    stdio: 'inherit'
+  });
+
+  //browserChild.on('close', () => {
+  //  console.log('close');
+  //  process.exit();
+  //});
+}, 200);
